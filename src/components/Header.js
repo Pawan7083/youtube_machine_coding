4a550988815c9519a5a01c172024d8aa559680df
Pawan Kumar <pawankr7083@gmail.com>
2025-08-03 23:55:42 +0530

    Header Component completed

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+const Header = ()=>{
+
+    return (
+        <div className="grid grid-flow-col p-4 shadow-lg ">
+            <div className="col-span-1 ">
+                <div className="flex p-2">
+                <i className="fa-solid fa-bars text-2xl cursor-pointer"></i>
+                <img className="h-8 w-40 ml-4 cursor-pointer" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e1/Logo_of_YouTube_%282015-2017%29.svg/2560px-Logo_of_YouTube_%282015-2017%29.svg.png" alt="logo" ></img>
+                </div>
+            </div>
+            <div className="col-span-9">
+                <input className="border border-gray-400 text-xl w-2/3 p-2 pl-4 rounded-l-3xl" type="text" ></input>
+                <button className="border border-gray-400 px-8 border-l-0 py-2 text-xl rounded-r-3xl hover:bg-gray-200"><i className="fa-solid fa-magnifying-glass"></i></button>    
+            </div>
+            <div className="col-span-1 ">
+                <div className="p-2  -mr-12 ">
+                    <span className="border border-gray-400 px-4 py-2 mr-4 rounded-2xl"> <i className="fa-solid fa-plus text"></i> Create</span>
+                <i class="fa-solid fa-bell mr-4 text-3xl"></i>
+                <i class="fa-solid fa-circle-user text-3xl"></i>
+                </div>
+            </div>
+            
+            
+        </div>
+    )
+}
+export default Header;
---
+++ src/components/SideMenu.js
@@ -0,0 +1,7 @@
+const SideMenu = ()=>{
+
+    return (
+        <div></div>
+    )
+}
+export default SideMenu;
